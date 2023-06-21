3085f2e818556ae66d3a4b07a6100a146c6ba98a
Cristian <crisvini.leoncini@outlook.com>
2023-06-21 11:28:59 -0300

    create, edit and delete frontend functions

--- index.php
+++ index.php
@@ -15,13 +15,13 @@
 
     <header class="header">
         <span class="text-8 fs-3">Tasks - <span class="kanban">Kanban board</span></span>
-        <button class="button-1"><i class="bi bi-plus-circle"></i>&nbsp;New task</button>
+        <button class="button-1" onclick="newTask({from: 'kanban'})"><i class="bi bi-plus-circle"></i>&nbsp;New task</button>
     </header>
 
     <main class="main background-7">
         <ul id="backlog" class="column">
             <div class="column-header backlog">
-                <span class="text-8"><i class="bi bi-journal"></i>&nbsp;Backlog</span>
+                <span><i class="bi bi-journal"></i>&nbsp;Backlog</span>
             </div>
             <li class="card grab">
                 <div class="card-title">
@@ -35,26 +35,25 @@
                         <span class="card-id"># 1</span>
                     </div>
                     <div class="card-footer-functions">
-                        <a id="view-1" class="link-1" onclick="viewTask({id: $(this).attr('id'), from: 'kanban'})"><i class="bi bi-eye"></i></a>
-                        <a id="edit-1" class="link-1" onclick="editTask({id: $(this).attr('id'), from: 'kanban'})"><i class="bi bi-pencil"></i></a>
-                        <a id="delete-1" class="link-1" onclick="deleteTask({id: $(this).attr('id'), from: 'kanban'})"><i class="bi bi-trash"></i></a>
+                        <a id="1" class="link-1" onclick="editTask({id: $(this).attr('id'), from: 'kanban'})"><i class="bi bi-pencil"></i></a>
+                        <a id="1" class="link-1" onclick="deleteTask({id: $(this).attr('id'), from: 'kanban'})"><i class="bi bi-trash"></i></a>
                     </div>
                 </div>
             </li>
         </ul>
         <ul id="to_do" class="column">
             <div class="column-header to-do">
-                <span class="text-8"><i class="bi bi-list-task"></i>&nbsp;To do</span>
+                <span><i class="bi bi-list-task"></i>&nbsp;To do</span>
             </div>
         </ul>
         <ul id="doing" class="column">
             <div class="column-header doing">
-                <span class="text-8"><i class="bi bi-clock"></i>&nbsp;Doing</span>
+                <span><i class="bi bi-clock"></i>&nbsp;Doing</span>
             </div>
         </ul>
         <ul id="done" class="column">
             <div class="column-header done">
-                <span class="text-8"><i class="bi bi-check2-circle"></i>&nbsp;Done</span>
+                <span><i class="bi bi-check2-circle"></i>&nbsp;Done</span>
             </div>
         </ul>
     </main>
--- js/common.js
+++ js/common.js
@@ -1,24 +1,84 @@
-function viewTask({ id, from }) {
-    console.log(id);
+async function newTask({ from }) {
+    await Swal.fire({
+        title: `New task`,
+        html: ` <div class="edit-task-div">
+                    <label class="custom-label" for="task_title">Title</label>
+                    <input class="custom-input" id="task_title">
+                    <label class="custom-label" for="task_description">Description</label>
+                    <textarea class="custom-textarea" id="task_description" rows="10"></textarea>
+                    <label class="custom-label" for="task_status">Status</label>
+                    <select class="custom-select" id="task_status">
+                        <option class="backlog-option" value="1">Backlog</option>
+                        <option class="to-do-option" value="2">To do</option>
+                        <option class="doing-option" value="3">Doing</option>
+                        <option class="done-option" value="4">Done</option>
+                    </select>
+                </div>`,
+        focusConfirm: false,
+        showDenyButton: true,
+        confirmButtonText: 'Create',
+        denyButtonText: 'Cancel',
+        customClass: {
+            title: 'custom-title',
+            actions: 'custom-actions',
+            htmlContainer: 'custom-htmlContainer'
+        },
+        preConfirm: () => {
+            return [
+                $('#task_title').val(),
+                $('#task_description').val(),
+                $('#task_status').val()
+            ]
+        }
+    })
 }
 
-function editTask({ id, from }) {
-    console.log(id);
+async function editTask({ id, from }) {
+    await Swal.fire({
+        title: `Edit task ${id}`,
+        html: ` <div class="edit-task-div">
+                    <label class="custom-label" for="task_title">Title</label>
+                    <input class="custom-input" id="task_title">
+                    <label class="custom-label" for="task_description">Description</label>
+                    <textarea class="custom-textarea" id="task_description" rows="10"></textarea>
+                    <label class="custom-label" for="task_status">Status</label>
+                    <select class="custom-select" id="task_status">
+                        <option class="backlog-option" value="1">Backlog</option>
+                        <option class="to-do-option" value="2">To do</option>
+                        <option class="doing-option" value="3">Doing</option>
+                        <option class="done-option" value="4">Done</option>
+                    </select>
+                </div>`,
+        focusConfirm: false,
+        showDenyButton: true,
+        denyButtonText: 'Cancel',
+        confirmButtonText: 'Save',
+        customClass: {
+            title: 'custom-title',
+            actions: 'custom-actions',
+            htmlContainer: 'custom-htmlContainer'
+        },
+        preConfirm: () => {
+            return [
+                $('#task_title').val(),
+                $('#task_description').val(),
+                $('#task_status').val()
+            ]
+        }
+    })
 }
 
 function deleteTask({ id, from }) {
     Swal.fire({
-        title: `Are you shure you want to delete task ${id}?`,
         icon: 'warning',
+        title: `Are you shure you want to delete task ${id}?`,
         showDenyButton: true,
         confirmButtonText: 'Yes',
         denyButtonText: 'Cancel',
+        position: 'bottom-end'
     }).then((result) => {
-        /* Read more about isConfirmed, isDenied below */
         if (result.isConfirmed) {
             Swal.fire('Saved!', '', 'success')
-        } else if (result.isDenied) {
-            Swal.fire('Changes are not saved', '', 'info')
         }
-    })
+    });
 }
